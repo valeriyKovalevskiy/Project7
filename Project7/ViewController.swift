8890ce74c5e5153d35185bc5ed11ada1e1564e13
Valeriy Kovalevskiy <v.kovalevskiy.ios@gmail.com>
2020-03-20 10:56:18 +0300

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Valeriy Kovalevskiy on 3/20/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
